47432ddfdf17779abb4334b688d1e9e4f51c32a6
Ljduley12 <127466421+Ljduley12@users.noreply.github.com>
2023-03-12 16:35:36 -0400

    Update spoofBlookProfile.js

--- stats/spoofBlookProfile.js
+++ stats/spoofBlookProfile.js
@@ -1,5 +1,5 @@
 (() => {
-    if(document.location.pathname !== "/chat") return;
+    if(document.location.pathname !== "/stats") return;
     blacket.user.blooks = blacket.blooks;
     $(".styles__headerBlookContainer___36zY5-camelCase").click()
     Object.keys(blacket.blooks).forEach((blook) => {
