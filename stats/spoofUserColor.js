4e437660bc966db6b8904f90d103734e39956ea2
Ljduley12 <127466421+Ljduley12@users.noreply.github.com>
2023-03-12 16:33:29 -0400

    Update spoofUserColor.js

--- stats/spoofUserColor.js
+++ stats/spoofUserColor.js
@@ -1,5 +1,5 @@
 (() => {
-    if(document.location.pathname !== "/stats") return;
+    if(document.location.pathname !== "/chat") return;
     $('head').append('<script src="https://cdn.jsdelivr.net/npm/@jaames/iro@5"></script>')
     $('.styles__bottomContainer___3kOrb-camelCase').append('<div class=styles__right___13qxc-camelCase><div class=styles__statsContainer___QnrRB-camelCase><div class=styles__containerHeader___3xghM-camelCase><div class=styles__containerHeaderInside___2omQm-camelCase>Color Picker</div></div><div class=styles__topStats___3qffP-camelCase id="picker"></div></div></div>')
     setTimeout(() => {
